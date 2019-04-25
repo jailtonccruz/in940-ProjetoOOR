655d5248217c70455c735286cf2746bc660fa4f7
Pedro Henrique <pedro00dk@gmail.com>
2019-04-25 14:34:56 -0300

    Update functions script

--- src/functions.sql
+++ src/functions.sql
@@ -1,15 +1,15 @@
---Funcionario Aumentar Salario--
-CREATE OR REPLACE TYPE BODY tp_Funcioanrio IS
-MEMBER FUNCTION salarioAumenta RETURN NUMBER IS
-BEGIN
+ALTER TYPE Tp_FuncionarioEfetivo ADD MEMBER FUNCTION orderPorSalario RETURN NUMBER CASCADE;
 
+CREATE OR REPLACE TYPE BODY Tp_FuncionarioEfetivo AS
+    MEMBER FUNCTION orderPorSalario RETURN NUMBER IS
+    BEGIN
+        RETURN salario;
+    END;
 END;
+/
 
-CREATE OR REPLACE TYPE BODY tp_Funcioanrio IS
-ORDER MEMBER FUNCTION comparaSalario (X tp_Funcionario)
-RETURN INTEGER IS
-BEGIN
 
-END;
 
+-- INSERT INTO TB_FUNCIONARIOEFETIVO VALUES (TP_FUNCIONARIOEFETIVO(123, 'NOME', AR_FONE(), TP_ENDERECO('', '', '', 'as'), TP_REL_EMITE(), 2222));
+-- select fe.orderPorSalario() from Tb_FuncionarioEfetivo fe
 
