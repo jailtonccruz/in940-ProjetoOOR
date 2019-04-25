786d9ff6920c3808706a57fc2b90ee2c2d202955
Elisa Andrade <elisandradecc@gmail.com>
2019-04-25 13:16:41 -0300

    Functions

---
+++ src/functions.sql
@@ -0,0 +1,15 @@
+--Funcionario Aumentar Salario--
+CREATE OR REPLACE TYPE BODY tp_Funcioanrio IS
+MEMBER FUNCTION salarioAumenta RETURN NUMBER IS
+BEGIN
+
+END;
+
+CREATE OR REPLACE TYPE BODY tp_Funcioanrio IS
+ORDER MEMBER FUNCTION comparaSalario (X tp_Funcionario)
+RETURN INTEGER IS
+BEGIN
+
+END;
+
+
