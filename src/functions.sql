9825a8ea2f823dcab8fee7518db487bab1395451
jailtonccruz <49525789+jailtonccruz@users.noreply.github.com>
2019-04-26 15:54:27 -0300

    Update functions.sql

--- src/functions.sql
+++ src/functions.sql
@@ -3,7 +3,7 @@ ALTER TYPE Tp_FuncionarioEfetivo ADD MEMBER FUNCTION orderPorSalario RETURN NUMB
 CREATE OR REPLACE TYPE BODY Tp_FuncionarioEfetivo AS
     MEMBER FUNCTION orderPorSalario RETURN NUMBER IS
     BEGIN
-        RETURN salario;
+        RETURN (salario);
     END;
 END;
 /
