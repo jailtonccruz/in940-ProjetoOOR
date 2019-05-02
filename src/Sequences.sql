0a45272d760b0243a4c786890a00dad80f0d8056
jailtonccruz <49525789+jailtonccruz@users.noreply.github.com>
2019-05-02 16:39:58 -0300

    Update Sequences.sql

--- src/Sequences.sql
+++ src/Sequences.sql
@@ -7,6 +7,7 @@ DROP SEQUENCE Sq_Departamento;
 DROP SEQUENCE Sq_Produto;
 DROP SEQUENCE Sq_Sessao;
 DROP SEQUENCE Sq_Pedido;
+DROP SEQUENCE Sq.Movimenta.NEXTVAL;
 
 -- Criando as Sequencias
 
@@ -15,7 +16,7 @@ CREATE SEQUENCE Sq_Departamento        START WITH 0 MINVALUE 0;
 CREATE SEQUENCE Sq_Produto             START WITH 0 MINVALUE 0;
 CREATE SEQUENCE Sq_Sessao              START WITH 0 MINVALUE 0;
 CREATE SEQUENCE Sq_Pedido              START WITH 0 MINVALUE 0;
-
+CREATE SEQUENCE Sq.Movimenta.NEXTVAL   START WITH 0 MINVALUE 0;
 -- Inicializand as Sequencias
 
 SELECT Sq_EmpresaTerceirizada.NEXTVAL FROM DUAL;
@@ -23,3 +24,4 @@ SELECT Sq_Departamento.NEXTVAL        FROM DUAL;
 SELECT Sq_Produto.NEXTVAL             FROM DUAL;
 SELECT Sq_Sessao.NEXTVAL              FROM DUAL;
 SELECT Sq_Pedido.NEXTVAL              FROM DUAL;
+SELECT Sq.Movimenta.NEXTVAL           FROM DUAL;
