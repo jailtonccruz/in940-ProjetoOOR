3a374c83aaf628e6a3a8426504f1e7faf1ae6f1b
jailtonccruz <49525789+jailtonccruz@users.noreply.github.com>
2019-05-02 16:31:47 -0300

    Create Sequences.sql

---
+++ src/Sequences.sql
@@ -0,0 +1,25 @@
+-- sequences
+
+-- Excluindo as Sequencias
+
+DROP SEQUENCE Sq_EmpresaTerceirizada;
+DROP SEQUENCE Sq_Departamento;
+DROP SEQUENCE Sq_Produto;
+DROP SEQUENCE Sq_Sessao;
+DROP SEQUENCE Sq_Pedido;
+
+-- Criando as Sequencias
+
+CREATE SEQUENCE Sq_EmpresaTerceirizada START WITH 0 MINVALUE 0;
+CREATE SEQUENCE Sq_Departamento        START WITH 0 MINVALUE 0;
+CREATE SEQUENCE Sq_Produto             START WITH 0 MINVALUE 0;
+CREATE SEQUENCE Sq_Sessao              START WITH 0 MINVALUE 0;
+CREATE SEQUENCE Sq_Pedido              START WITH 0 MINVALUE 0;
+
+-- Inicializand as Sequencias
+
+SELECT Sq_EmpresaTerceirizada.NEXTVAL FROM DUAL;
+SELECT Sq_Departamento.NEXTVAL        FROM DUAL;
+SELECT Sq_Produto.NEXTVAL             FROM DUAL;
+SELECT Sq_Sessao.NEXTVAL              FROM DUAL;
+SELECT Sq_Pedido.NEXTVAL              FROM DUAL;
