20b3159bc09a7ad967f87080a04099a8c0312c00
jailtonccruz <49525789+jailtonccruz@users.noreply.github.com>
2019-05-02 17:50:38 -0300

    Update tables.sql

--- src/tables.sql
+++ src/tables.sql
@@ -63,7 +63,8 @@ CREATE TABLE Tb_Rel_Inclui OF Tp_Rel_Inclui (
 CREATE TABLE Tb_Rel_Movimenta OF Tp_Rel_Movimenta (
     cod        PRIMARY KEY,
     data       NOT NULL,
-    quantidade NOT NULL
+    quantidade NOT NULL,
+    tipo       NOT NULL
 );
 
 ---- DROPS ----
