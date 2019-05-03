03145559a661289c0750f6474a64a4c88b276e74
Karinegmg <kgmg@cin.ufpe.br>
2019-05-03 14:46:51 -0300

    fixing query

--- src/queries.sql
+++ src/queries.sql
@@ -1,6 +1,6 @@
 --SUBCONSULTA COM IN (pegando os produtos que foram vendidos no 1o dia do ano)
-select prod.id from tb_produto prod
-where prod.id in (select ped.id from tb_pedido ped
-where ped.id = prod.id and ped.data = '01/01/2019');
+select prod.cod from tb_produto prod
+where prod.cod in (select ped.cod from tb_pedido ped
+where ped.cod = prod.cod and ped.data = '01/01/2018');
 
 -- SUBCONSULTA COM ANY 
