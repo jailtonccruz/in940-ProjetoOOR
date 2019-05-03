c44dad2ae092dbdf3fd4a8f1ffca14a61ce2937f
Pedro Henrique <pedro00dk@gmail.com>
2019-05-03 19:51:55 -0300

    Create functions to create departments and register employees on it

---
+++ src/bodies/departamento.sql
@@ -0,0 +1,63 @@
+ALTER TYPE Tp_Departamento
+    ADD STATIC PROCEDURE cadastraDepartamento (cod_ NUMBER, nome_ VARCHAR2, sigla_ VARCHAR2),
+    ADD MEMBER PROCEDURE registraNoDepartamento (funcionario_ Tp_Funcionario)
+    CASCADE;
+/
+
+CREATE OR REPLACE TYPE BODY Tp_Departamento AS
+
+    -- Cadastra um novo departamento na base de dados
+    STATIC PROCEDURE cadastraDepartamento (cod_ NUMBER, nome_ VARCHAR2, sigla_ VARCHAR2) IS
+        BEGIN
+            INSERT INTO Tb_Departamento VALUES (Tp_Departamento(cod_, nome_, sigla_, Tp_Rel_Pertence()));
+            DBMS_OUTPUT.PUT_LINE('departamento criado:' || nome_);
+        END;
+
+
+    -- Adiciona o funcionario neste departamento, se o funcionario já está em outro departamento, ele é removido dele.
+    MEMBER PROCEDURE registraNoDepartamento (funcionario_ Tp_Funcionario) IS
+        emDepartamento NUMBER := 0;
+        CURSOR departamentos IS (
+            SELECT *
+            FROM Tb_Departamento
+        );
+        departamentoAtual departamentos%ROWTYPE := NULL;
+        emEfetivo NUMBER := 0;
+        BEGIN
+            -- find previous employee's department
+            FOR departamento IN departamentos
+                LOOP
+                    SELECT COUNT(*) into emDepartamento
+                    FROM TABLE (departamento.pertence) t
+                    WHERE t.funcionario.cpf = funcionario_.cpf;
+                    IF emDepartamento > 0 THEN
+                        departamentoAtual := departamento;
+                        EXIT;
+                    END IF;
+                END LOOP;
+
+            -- remove employee from the old department nested table
+            IF departamentoAtual.cod IS NOT NULL THEN
+                DBMS_OUTPUT.PUT_LINE('removendo do departamento: ' || departamentoAtual.nome);
+                DELETE
+                FROM TABLE (SELECT d.pertence FROM Tb_Departamento d WHERE d.cod = departamentoAtual.cod) t
+                WHERE t.funcionario.cpf = funcionario_.cpf;
+            END IF;
+
+            -- add the employee to the new department nested table and update
+            SELECT COUNT(*) INTO emEfetivo FROM Tb_FuncionarioEfetivo f WHERE f.cpf = funcionario_.cpf;
+            IF emEfetivo <> 0 THEN
+                DBMS_OUTPUT.PUT_LINE('funcionario do tipo efetivo');
+                INSERT INTO TABLE (SELECT d.pertence FROM Tb_Departamento d WHERE d.cod = SELF.cod)
+                VALUES ((SELECT ref(f) FROM Tb_FuncionarioEfetivo f WHERE f.cpf = funcionario_.cpf));
+            ELSE
+                DBMS_OUTPUT.PUT_LINE('funcionario do tipo terceirizado');
+                INSERT INTO TABLE (SELECT d.pertence FROM Tb_Departamento d WHERE d.cod = SELF.cod)
+                VALUES ((SELECT ref(f) FROM Tb_FuncionarioTerceirizado f WHERE f.cpf = funcionario_.cpf));
+            END IF;
+            DBMS_OUTPUT.PUT_LINE('adicionado ao departamento: ' || nome);
+
+        END;
+
+END;
+/
