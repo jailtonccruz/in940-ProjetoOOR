5e2007fd71ad5ebcde46e9fc8c0b417c1fb19cc7
Pedro Henrique <pedro00dk@gmail.com>
2019-05-04 13:38:02 -0300

    Add logs to create customers procedure

--- src/bodies/cliente.sql
+++ src/bodies/cliente.sql
@@ -1,6 +1,5 @@
 ALTER TYPE Tp_Cliente
-    ADD STATIC PROCEDURE cadastra (cpf_ NUMBER, nome_ VARCHAR2, telefone_ NUMBER, endereco_),
-    ADD MEMBER FUNCTION checaFreteGratis RETURN BOOLEAN,
+    ADD STATIC PROCEDURE cadastra (cpf_ NUMBER, nome_ VARCHAR2, telefone_ NUMBER, endereco_ Tp_Endereco),
     ADD MEMBER FUNCTION valorGasto (de DATE, ate DATE) RETURN NUMBER
     CASCADE;
 /
@@ -8,31 +7,23 @@ ALTER TYPE Tp_Cliente
 CREATE OR REPLACE TYPE BODY Tp_Cliente AS
 
 
-    -- Cadastra um novo cliente.
+    -- Cadastra um novo cliente
     STATIC PROCEDURE cadastra (cpf_ NUMBER, nome_ VARCHAR2, telefone_ NUMBER, endereco_ Tp_Endereco) IS
         BEGIN
             INSERT INTO Tb_Cliente
-            VALUES (
-                       Tp_Cliente(cpf_, nome_, Ar_Fone(telefone_), endereco_, Tp_Rel_Emite())
-                   );
+            VALUES (Tp_Cliente(cpf_, nome_, Ar_Fone(telefone_), endereco_, Tp_Rel_Faz()));
+            DBMS_OUTPUT.PUT_LINE('cliente criado');
+            DBMS_OUTPUT.PUT_LINE('    cpf: ' || cpf_);
+            DBMS_OUTPUT.PUT_LINE('    nome: ' || nome_);
         END;
 
 
-    -- Function para checar se o cliente tem direito a frete grátis (UF = PE)
-    MEMBER FUNCTION checaFreteGratis RETURN BOOLEAN IS
+    -- Calcula o valor gasto pelo cliente dado o intervalo de dadas
+    MEMBER FUNCTION valorGasto (de DATE, ate DATE) RETURN NUMBER IS
+        valorGasto NUMBER := 0;
         BEGIN
-
-            RETURN ENDERECO.UF = 'PE';
-        END;
-
-
-    MEMBER FUNCTION valorGasto (de DATE, ate DATE) RETURN BOOLEAN IS
-        valorGasto NUMBER;
-        BEGIN
-            valorGasto := 0;
-
-            SELECT * FROM TABLE (faz);
-            --SELECT SUM(i.valorTotal()) INTO valorGasto FROM (TABLE (faz) f INNER JOIN Tb_Rel_Inclui ri ON f.faz.cod = ri.pedido.pedido.cod) i;
+            --SELECT SUM(i.valorTotal()) INTO valorGasto
+            --FROM (TABLE (faz) f INNER JOIN Tb_Rel_Inclui ri ON f.pedido.cod = ri.pedido.pedido.cod) i
             --WHERE f.faz.data BETWEEN de AND ate;
             RETURN valorGasto;
         END;
