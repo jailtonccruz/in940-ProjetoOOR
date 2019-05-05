37249779516f7bbcfdd07daa6508517333fc2f6d
Pedro Henrique <pedro00dk@gmail.com>
2019-05-05 11:16:57 -0300

    Implement map function to order people by name.

--- src/bodies/pessoa.sql
+++ src/bodies/pessoa.sql
@@ -1,1 +1,14 @@
--- TODO implementar order by nome
+ALTER TYPE Tp_Pessoa
+    ADD MAP MEMBER FUNCTION mapNome RETURN VARCHAR2
+    CASCADE;
+/
+
+CREATE OR REPLACE TYPE BODY Tp_Pessoa AS
+
+    -- Map function to order by name
+    MAP MEMBER FUNCTION mapNome RETURN VARCHAR2 IS
+        BEGIN
+            RETURN SELF.nome;
+        END;
+END;
+/
