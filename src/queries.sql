d3102bf4cc9ee97361babca82b0c060037099364
jailtonccruz <49525789+jailtonccruz@users.noreply.github.com>
2019-05-06 19:20:27 -0300

    Update queries.sql

--- src/queries.sql
+++ src/queries.sql
@@ -1,6 +1,9 @@
---SUBCONSULTA COM IN (pegando os produtos que foram vendidos no 1o dia do ano)
-select prod.cod from tb_produto prod
-where prod.cod in (select ped.cod from tb_pedido ped
-where ped.cod = prod.cod and ped.data = '01/01/2018');
+--SUBCONSULTA COM IN (pegando os produtos que NÃO foram vendidos)
+
+SELECT pro.cod, pro.nome
+  FROM tb_produto pro
+ WHERE pro.cod NOT IN
+(select DEREF(r.produto).cod
+   FROM tb_rel_inclui r)
 
 -- SUBCONSULTA COM ANY 
